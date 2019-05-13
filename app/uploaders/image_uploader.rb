63f9d617f536aa6a7d354855b8baceddd3ace2f8
ryo <0823.tr@gmail.com>
2019-05-13 11:54:30 +0900

    本番環境で画像投稿できるように設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,8 +3,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [300, 200]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :file
+  else
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
