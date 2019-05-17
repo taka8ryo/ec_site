6fa05e3b4ae9c5e860a2be06b27bf8f41bdcb6c3
ryo <0823.tr@gmail.com>
2019-05-17 21:23:58 +0900

    決済機能の追加(途中)

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,13 @@
+class ChargesController < ApplicationController
+  def create
+    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+    token = params[:stripeToken]
+    Stripe::Charge.create({
+      amount: @total_price ,
+      currency: 'jpy',
+      description: 'Example charge',
+      source: token
+    })
+    redirect_to root_path, notice: "決済に成功しました"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   resource :basket, only: [:show]
+  resource :charge, only: [:create]
   resources :products, only: [:new, :show, :create] do
     scope module: :products do
       resources :add_to_baskets, only: [:create]
