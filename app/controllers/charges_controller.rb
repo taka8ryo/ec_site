dbc1d79f360c5c9c34feb3db04853de6cbb3667b
ryo <0823.tr@gmail.com>
2019-05-21 20:47:16 +0900

    決済機能（完了）

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,8 +2,12 @@ class ChargesController < ApplicationController
   def create
     Stripe.api_key = ENV['STRIPE_SECRET_KEY']
     token = params[:stripeToken]
+    product_ids = params[:product_ids].map(&:to_i)
+
+    products = current_user.basket.products.where(id: product_ids)
+    total = products.where(unit: :yen).sum(:price)
     Stripe::Charge.create({
-      amount: @total_price,
+      amount: total,
       currency: 'jpy',
       description: 'Example charge',
       source: token
